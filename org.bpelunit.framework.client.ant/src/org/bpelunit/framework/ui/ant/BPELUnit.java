a0659d78db849b82a321651d8ce5db7e469b0ca4
Alex Salnikow <none>
2007-08-07 21:52:55 +0000

    *** empty log message ***

---
+++ org.bpelunit.framework.client.ant/src/org/bpelunit/framework/ui/ant/BPELUnit.java
@@ -0,0 +1,251 @@
+/**
+ * This file belongs to the BPELUnit utility and Eclipse plugin set. See enclosed
+ * license file for more information.
+ * 
+ */
+package org.bpelunit.framework.ui.ant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.bpelunit.framework.BPELUnitRunner;
+import org.bpelunit.framework.exception.ConfigurationException;
+import org.bpelunit.framework.exception.DeploymentException;
+import org.bpelunit.framework.exception.SpecificationException;
+
+/**
+ * <p>
+ * BPELUnit Ant Task
+ * </p>
+ * 
+ * <p>
+ * Invocation:
+ * 
+ * <pre>
+ *     &lt;bpelunit testsuite=&quot;testsuite.bpts&quot; haltOnError=&quot;true&quot; haltOnFailure=&quot;true&quot; bpelunitdir=&quot;c:\location&quot;&gt; 
+ *     &lt;output type=&quot;plain&quot; file=&quot;false&quot; /&gt;
+ *     &lt;logging level=&quot;plain&quot; file=&quot;false&quot; /&gt;
+ *     &lt;/bpelunit&gt;
+ * </pre>
+ * 
+ * </p>
+ * 
+ * @version $Id$
+ * @author Philip Mayer
+ * 
+ */
+public class BPELUnit extends Task {
+
+	/**
+	 * The test suite path and file name
+	 */
+	private String fTestSuite;
+
+	/**
+	 * The BPELUnit home directory
+	 */
+	private String fBPELUnitDir;
+
+	/**
+	 * If true, the framework halts on errors
+	 */
+	private boolean fHaltOnError;
+
+	/**
+	 * if true, the framework halts on failures
+	 */
+	private boolean fHaltOnFailure;
+
+	/**
+	 * List of "outputters"
+	 */
+	private List<Output> fOutputList= new ArrayList<Output>();
+
+	/**
+	 * List of "loggers"
+	 */
+	private List<Logging> fLoggingList= new ArrayList<Logging>();
+
+	@Override
+	public void execute() {
+
+		// Check attributes
+
+		if (fTestSuite == null || fTestSuite.equals(""))
+			throw new BuildException("The testsuite argument is required.");
+
+		File testSuite= new File(fTestSuite);
+		if (!testSuite.exists())
+			throw new BuildException("The testsuite file does not exist.");
+
+		if (fBPELUnitDir == null || fBPELUnitDir.equals(""))
+			fBPELUnitDir= System.getenv("BPELUNIT_HOME");
+
+		if (fBPELUnitDir == null || fBPELUnitDir.equals(""))
+			throw new BuildException("The BPELUnit directory is required - either specify an argument or set environment variable BPELUNIT_HOME.");
+
+		for (Output output : fOutputList) {
+			if (!output.hasValidStyle())
+				throw new BuildException("An output specification is missing the style attribute, or wrong style specified.");
+		}
+
+		for (Logging logging : fLoggingList) {
+			if (!logging.hasValidLevel())
+				throw new BuildException("A logging specification is missing the level attribute, or wrong level specified.");
+		}
+
+		HashMap<String, String> options= new HashMap<String, String>();
+		options.put(BPELUnitRunner.HALT_ON_ERROR, Boolean.toString(fHaltOnError));
+		options.put(BPELUnitRunner.HALT_ON_FAILURE, Boolean.toString(fHaltOnFailure));
+
+		BPELUnitAntRunner runner= new BPELUnitAntRunner(fBPELUnitDir, fLoggingList, fOutputList);
+		try {
+			runner.initialize(options);
+			runner.run(testSuite);
+		} catch (ConfigurationException e) {
+			throw new BuildException(e.getMessage(), e);
+		} catch (SpecificationException e) {
+			throw new BuildException(e.getMessage(), e);
+		} catch (DeploymentException e) {
+			throw new BuildException(e.getMessage(), e);
+		}
+
+	}
+
+	public void setTestSuite(String testSuite) {
+		fTestSuite= testSuite;
+	}
+
+	public void setBPELUnitDir(String bpelUnitDir) {
+		fBPELUnitDir= bpelUnitDir;
+	}
+
+	public void setHaltOnError(boolean haltOnError) {
+		fHaltOnError= haltOnError;
+	}
+
+	public void setHaltOnFailure(boolean haltOnFailure) {
+		fHaltOnFailure= haltOnFailure;
+	}
+
+	public void addConfiguredOutput(Output output) {
+		fOutputList.add(output);
+	}
+
+	public void addConfiguredLogging(Logging logging) {
+		fLoggingList.add(logging);
+	}
+
+
+	public static class Output extends Extension {
+
+		public static final String STYLE_PLAIN= "PLAIN";
+		public static final String STYLE_XML= "XML";
+
+		private String fStyle;
+
+		public String getStyle() {
+			return fStyle;
+		}
+
+		public void setStyle(String style) {
+			fStyle= style;
+		}
+
+
+		public boolean hasValidStyle() {
+			return STYLE_PLAIN.equals(fStyle) || STYLE_XML.equals(fStyle);
+		}
+	}
+
+	public static class Extension {
+
+		private String fFile;
+		private OutputStream fOutput;
+
+		public OutputStream getOutput() {
+			return fOutput;
+		}
+
+		public void initialize() throws ConfigurationException {
+			fOutput= createOutputStream();
+		}
+
+		public void dispose() {
+			if (fOutput != null) {
+				try {
+					fOutput.flush();
+					fOutput.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+
+		private OutputStream createOutputStream() throws ConfigurationException {
+			if (fFile != null) {
+				String dir= FilenameUtils.getPath(fFile);
+				if (!"".equals(dir))
+					new File(dir).mkdirs();
+
+				try {
+					return new FileOutputStream(fFile);
+				} catch (FileNotFoundException e) {
+					throw new ConfigurationException("Cannot create file with name " + fFile + " for output.");
+				}
+			} else
+				return System.out;
+		}
+
+		public void write(String line) throws IOException {
+			fOutput.write(line.getBytes());
+		}
+
+		public void setFile(String file) {
+			fFile= file;
+		}
+
+		public String getFile() {
+			return fFile;
+		}
+	}
+
+	public static class Logging extends Extension {
+
+		private String fLevel;
+
+		public boolean hasValidLevel() {
+			if (fLevel == null)
+				return false;
+			try {
+				Level.parse(fLevel);
+				return true;
+			} catch (IllegalArgumentException e) {
+				return false;
+			}
+		}
+
+		public void setLevel(String type) {
+			fLevel= type;
+		}
+
+		public String getLevel() {
+			return fLevel;
+		}
+
+		public OutputStream getOutputStream() {
+			return getOutput();
+		}
+	}
+
+}
---
+++ org.bpelunit.framework.client.ant/src/org/bpelunit/framework/ui/ant/BPELUnitAntRunner.java
@@ -0,0 +1,217 @@
+/**
+ * This file belongs to the BPELUnit utility and Eclipse plugin set. See enclosed
+ * license file for more information.
+ * 
+ */
+package org.bpelunit.framework.ui.ant;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.WriterAppender;
+import org.apache.log4j.varia.NullAppender;
+import org.bpelunit.framework.base.BPELUnitBaseRunner;
+import org.bpelunit.framework.control.result.ITestResultListener;
+import org.bpelunit.framework.control.result.XMLResultProducer;
+import org.bpelunit.framework.exception.ConfigurationException;
+import org.bpelunit.framework.exception.DeploymentException;
+import org.bpelunit.framework.exception.SpecificationException;
+import org.bpelunit.framework.model.test.PartnerTrack;
+import org.bpelunit.framework.model.test.TestCase;
+import org.bpelunit.framework.model.test.TestSuite;
+import org.bpelunit.framework.model.test.report.ITestArtefact;
+import org.bpelunit.framework.ui.ant.BPELUnit.Logging;
+import org.bpelunit.framework.ui.ant.BPELUnit.Output;
+
+/**
+ * The ant implementation of the BPELUnit runner. Offers support for outputting logging and test
+ * results to file or sysout.
+ * 
+ * @version $Id$
+ * @author Philip Mayer
+ * 
+ */
+public class BPELUnitAntRunner extends BPELUnitBaseRunner implements ITestResultListener {
+
+	/**
+	 * The home directory of BPELUnit.
+	 */
+	private String fBPELUnitHomeDirectory;
+
+	/**
+	 * List of "loggers"
+	 */
+	private List<Logging> fLoggers;
+
+	/**
+	 * List of "outputters"
+	 */
+	private List<Output> fOutputs;
+
+	/**
+	 * Failure count.
+	 */
+	private int fFailures;
+
+	/**
+	 * Error count.
+	 */
+	private int fErrors;
+
+	/**
+	 * Run count.
+	 */
+	private int fRuns;
+
+	// ********************* Initialization ********************
+
+	public BPELUnitAntRunner(String homedir, List<Logging> loggers, List<Output> outputList) {
+		fBPELUnitHomeDirectory= homedir;
+		fLoggers= loggers;
+		fOutputs= outputList;
+		fFailures= 0;
+		fErrors= 0;
+		fRuns= 0;
+	}
+
+	@Override
+	public void configureInit() throws ConfigurationException {
+		setHomeDirectory(fBPELUnitHomeDirectory);
+
+		for (Output output : fOutputs)
+			output.initialize();
+	}
+
+	@Override
+	public void configureLogging() throws ConfigurationException {
+
+		Logger.getRootLogger().removeAllAppenders();
+		for (Logging log : fLoggers) {
+			log.initialize();
+			Logger.getRootLogger().addAppender(new WriterAppender(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN), log.getOutputStream()));
+			Logger.getRootLogger().setLevel(Level.toLevel(log.getLevel()));
+		}
+
+		if (fLoggers.isEmpty())
+			Logger.getRootLogger().addAppender(new NullAppender());
+	}
+
+	// ************************* Running *************************
+
+	public void run(File testSuite) throws SpecificationException, DeploymentException {
+
+		TestSuite suite= loadTestSuite(testSuite);
+		suite.addResultListener(this);
+
+		try {
+			suite.setUp();
+		} catch (DeploymentException e) {
+			try {
+				suite.shutDown();
+			} catch (DeploymentException e1) {
+			}
+			throw e;
+		}
+
+		outputPlain("START", suite);
+
+		suite.run();
+		suite.removeResultListener(this);
+
+		outputPlain("END", suite);
+
+		String result= "Test Run Completed. " + fRuns + " " + getPluralOf(fRuns, "run") + " (" + fFailures + " " + getPluralOf(fFailures, "failure")
+				+ ", " + fErrors + " " + getPluralOf(fErrors, "error") + ") \n";
+		outputPlain(result);
+		System.out.println(result);
+
+		outputXML(suite);
+
+		closeOutputs();
+
+		suite.shutDown();
+
+	}
+
+	// *********************** ITestResultListener *********************
+
+	public void testCaseStarted(TestCase testCase) {
+		outputPlain("START", testCase);
+	}
+
+	public void testCaseEnded(TestCase testCase) {
+		if (testCase.getStatus().isFailure())
+			fFailures++;
+		if (testCase.getStatus().isError())
+			fErrors++;
+		fRuns++;
+
+		outputPlain("END", testCase);
+	}
+
+	public void progress(ITestArtefact testArtefact) {
+		if (testArtefact instanceof PartnerTrack)
+			outputPlain("PROGRESS", testArtefact);
+	}
+
+	private void outputPlain(String head, ITestArtefact testCase) {
+		String info= head + ": " + testCase.getName() + ": " + testCase.getStatus().toString() + "\n";
+		outputPlain(info);
+	}
+
+	// ************************** Helpers *************************
+
+	private void outputPlain(String info) {
+		for (Iterator<Output> i= fOutputs.iterator(); i.hasNext();) {
+			Output output= i.next();
+			try {
+				if (output.getStyle().equals(Output.STYLE_PLAIN))
+					output.write(info);
+			} catch (IOException e) {
+				System.out.println("I/O Error writing to output stream - canceling output.");
+				output.dispose();
+				i.remove();
+			}
+		}
+	}
+
+	private void outputXML(TestSuite suite) {
+		for (Iterator<Output> i= fOutputs.iterator(); i.hasNext();) {
+			Output output= i.next();
+			try {
+				if (output.getStyle().equals(Output.STYLE_XML)) {
+					XMLResultProducer.writeXML(output.getOutput(), suite);
+				}
+			} catch (IOException e) {
+				System.out.println("I/O Error writing to output stream - canceling output.");
+				output.dispose();
+				i.remove();
+			}
+		}
+	}
+
+	private String getPluralOf(int no, String name) {
+		return no == 1 ? name : name + "s";
+	}
+
+	private void closeOutputs() {
+		for (Logging l : fLoggers)
+			l.dispose();
+
+		for (Output o : fOutputs)
+			o.dispose();
+	}
+
+	@Override
+	public void configureCoverageTool() throws ConfigurationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
