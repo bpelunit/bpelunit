c36ca83b3b7a14e3aa6e24b2ffa263a8850bd26c
Daniel Luebke <bpelunit@daniel-luebke.de>
2009-03-06 23:45:01 +0000

    Building works for me (DL)

---
+++ org.bpelunit.build.standalone/build/bpelunit.bat
@@ -0,0 +1,29 @@
+@echo off
+REM *********** BPELUnit Runner *************
+REM Expects the following ENV VARIABLES TO BE SET
+REM * JAVA_HOME
+REM * BPELUNIT_HOME
+REM to be set
+
+setlocal
+
+if not defined JAVA_HOME echo ERROR: Environment variable JAVA_HOME is undefined & goto :eof
+if not defined BPELUNIT_HOME echo ERROR: Environment variable BPELUNIT_HOME is undefined & goto :eof
+
+set LIB_HOME="%BPELUNIT_HOME%\lib"
+set CLASSPATH=
+    
+for %%1 in (%LIB_HOME%\*.jar) do call :addItem %%~f1
+
+goto :run
+
+:run
+REM Classpath is set up, go run bpelunit
+
+%JAVA_HOME%\bin\java org.bpelunit.framework.ui.command.BPELUnitCommandLineRunner %*
+
+endlocal
+goto :eof
+
+:addItem
+set CLASSPATH=%CLASSPATH%;%1
