6d9737cdd50fccdc4380e649f2d2cb89cbe8acef
Antonio Garcia Dominguez <none>
2010-02-18 09:56:35 +0000

    ProcessListRequestEntity: filter optionally by name, too
    From: Antonio García <antonio.garciadominguez@uca.es>

--- org.bpelunit.framework/src/org/bpelunit/framework/control/deploy/activebpel/ProcessListRequestEntity.java
+++ org.bpelunit.framework/src/org/bpelunit/framework/control/deploy/activebpel/ProcessListRequestEntity.java
@@ -11,10 +11,14 @@ import org.w3c.dom.DOMException;
 
 /**
  * Request entity for getting the list of all running WS-BPEL processes from the
- * ActiveBPEL 4.1 administration WS.
+ * ActiveBPEL 4.1 administration WS. Optionally, we can select only those from
+ * a particular WS-BPEL composition. This will be useful when tearing down the
+ * test suite or cleaning up after each test case: we only want the IDs of the
+ * processes we want to kill, without affecting other processes which might be
+ * running.
  *
  * @author Antonio García Domínguez
- * @version 1.0 2009/05/02
+ * @version 1.1 2009/11/17
  */
 public class ProcessListRequestEntity extends ActiveBPELRequestEntityBase {
 
@@ -22,8 +26,27 @@ public class ProcessListRequestEntity extends ActiveBPELRequestEntityBase {
     private static final String ACTIVEBPEL_PLIST_FILTER_PSTATE = "processState";
     private static final String ACTIVEBPEL_PLIST_FILTER        = "filter";
     private static final String ACTIVEBPEL_PLISTINPUT          = "getProcessListInput";
+    private static final String ACTIVEBPEL_PLIST_FILTER_PNAME  = "processName";
 
+    private String processName;
+
+    /**
+     * Convenience constructor which creates a request for the list of all
+     * running WS-BPEL processes. It does not take into account which
+     * process definition these were instantiated from.
+     */
     public ProcessListRequestEntity() throws IOException, SOAPException {
+        this(null);
+    }
+
+    /**
+     * Creates a request for the list of all running WS-BPEL processes which
+     * instantiate a particular process definition. The name must be exactly
+     * the same in the .bpel and .bpts files.
+     */
+    public ProcessListRequestEntity(String processName)
+      throws IOException, SOAPException {
+        this.processName = processName;
         createMessage();
     }
 
@@ -36,11 +59,20 @@ public class ProcessListRequestEntity extends ActiveBPELRequestEntityBase {
         SOAPElement filterElement = rootElement.addChildElement(
             new QName(ActiveBPELRequestEntityBase.NS_ACTIVEBPEL_ADMIN,
                       ACTIVEBPEL_PLIST_FILTER));
+
+        // Filter by state (running)
         SOAPElement pstateElement = filterElement.addChildElement(
             new QName(ActiveBPELRequestEntityBase.NS_ACTIVEBPEL_ADMIN,
                       ACTIVEBPEL_PLIST_FILTER_PSTATE));
-
         pstateElement.setTextContent(ACTIVEBPEL_PSTATE_RUNNING);
+
+        // Filter by name, if processName has been set
+        if (processName != null) {
+            SOAPElement pnameElement = filterElement.addChildElement(
+                new QName(ActiveBPELRequestEntityBase.NS_ACTIVEBPEL_ADMIN,
+                          ACTIVEBPEL_PLIST_FILTER_PNAME));
+            pnameElement.setTextContent(processName);
+        }
     }
 
 }
