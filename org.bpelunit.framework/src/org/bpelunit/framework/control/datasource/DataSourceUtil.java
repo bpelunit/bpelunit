98cf6ea55d470313662e719da2b40fd7918ae586
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2010-04-08 01:33:04 +0800

    DataSourceUtil: create IDataSources from XMLBeans objects

--- org.bpelunit.framework/src/org/bpelunit/framework/control/datasource/DataSourceUtil.java
+++ org.bpelunit.framework/src/org/bpelunit/framework/control/datasource/DataSourceUtil.java
@@ -2,17 +2,109 @@ package org.bpelunit.framework.control.datasource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import org.bpelunit.framework.control.ext.IDataSource;
+import org.bpelunit.framework.control.util.ExtensionRegistry;
 import org.bpelunit.framework.exception.DataSourceException;
+import org.bpelunit.framework.exception.SpecificationException;
 import org.bpelunit.framework.xml.suite.XMLDataSource;
+import org.bpelunit.framework.xml.suite.XMLProperty;
+import org.bpelunit.framework.xml.suite.XMLTestCase;
+import org.bpelunit.framework.xml.suite.XMLTestSuite;
 
 /**
  * Utility methods for working with data sources.
  * 
  * @author Antonio García Domínguez
- * @version 1.0
+ * @version 1.1
  */
 public class DataSourceUtil {
+
+	/**
+	 * Creates the effective IDataSource for a test case inside a test suite. It
+	 * is a convenience wrapper for
+	 * {@link #createDataSource(String, InputStream, Map)}.
+	 * 
+	 * @param xmlTestSuite
+	 *            XMLBeans object for the <testSuite> element.
+	 * @param xmlTestCase
+	 *            XMLBeans object for the <testCase> element.
+	 * @return <code>null</code> if no data source is available. Otherwise, an
+	 *         instance of the appropriate data source will be returned, with
+	 *         its contents initialized from its source according to
+	 *         {@link #getStreamForDataSource(XMLDataSource)} and all specified
+	 *         properties set.
+	 * @throws DataSourceException
+	 *             The requested data source type is not known, or there was a
+	 *             problem while initializing its contents from the XML element.
+	 */
+	public static IDataSource createDataSource(XMLTestSuite xmlTestSuite,
+			XMLTestCase xmlTestCase) throws DataSourceException {
+		XMLDataSource xmlDataSource = null;
+		if (xmlTestCase.isSetSetUp()
+				&& xmlTestCase.getSetUp().isSetDataSource()) {
+			xmlDataSource = xmlTestCase.getSetUp().getDataSource();
+		} else if (xmlTestSuite.isSetSetUp()
+				&& xmlTestSuite.getSetUp().isSetDataSource()) {
+			xmlDataSource = xmlTestSuite.getSetUp().getDataSource();
+		}
+
+		if (xmlDataSource != null) {
+			final String type = xmlDataSource.getType();
+			final InputStream istream = getStreamForDataSource(xmlDataSource);
+			Map<String, String> properties = createPropertyMap(xmlDataSource
+					.getPropertyList());
+			return createDataSource(type, istream, properties);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Creates and initializes an IDataSource instance.
+	 * 
+	 * @param type
+	 *            Type of the data source instance, according to the extension
+	 *            registry in <code>conf/extensions.xml</code>.
+	 * @param istream
+	 *            InputStream with the contents to be loaded, as created by
+	 *            {@link #getStreamForDataSource(String, String)} or its
+	 *            wrappers.
+	 * @param properties
+	 *            Key-value map for the properties to be set on the data source.
+	 * @return IDataSource instance with its contents initialized from the
+	 *         InputStream and all properties set.
+	 * @throws DataSourceException
+	 *             There was a problem while creating the data source instance,
+	 *             loading its contents or setting a property.
+	 */
+	public static IDataSource createDataSource(final String type,
+			final InputStream istream, Map<String, String> properties)
+			throws DataSourceException {
+		IDataSource dataSource = null;
+
+		try {
+			dataSource = ExtensionRegistry.createNewDataSourceForType(type);
+		} catch (SpecificationException e) {
+			throw new DataSourceException(
+					"Could not create data source instance for type " + type, e);
+		}
+
+		for (Entry<String, String> prop : properties.entrySet()) {
+			dataSource.setProperty(prop.getKey(), prop.getValue());
+		}
+
+		// Contents should be loaded after setting the properties, as these might
+		// have information that is required to correctly interpret the input
+		// stream, such as text encoding, for instance.
+		dataSource.loadFromStream(istream);
+
+		return dataSource;
+	}
 
 	/**
 	 * Returns an InputStream for the contents referenced in the data source. It
@@ -66,4 +158,14 @@ public class DataSourceUtil {
 		}
 	}
 
+	/************* PRIVATE METHODS ****************/
+
+	private static Map<String, String> createPropertyMap(
+			List<XMLProperty> propertyList) {
+		HashMap<String, String> map = new HashMap<String, String>();
+		for (XMLProperty prop : propertyList) {
+			map.put(prop.getName(), prop.getStringValue());
+		}
+		return map;
+	}
 }
