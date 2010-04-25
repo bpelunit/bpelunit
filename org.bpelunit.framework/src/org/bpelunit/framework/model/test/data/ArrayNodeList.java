d203cececa0d11df3bae5908cdebc5d32fc1be8a
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2010-04-25 23:34:23 +0800

    New XPathVariableResolver for Velocity contexts
    To transparently access the variables of the activity context inside
    receive conditions, a proper XPath variable resolver has to be provided. This
    commit implements that provider.
    
    The provider mostly maps XPath variables to Velocity context variables, except
    for one thing. The Xalan engine doesn't map automatically ArrayList objects to
    node lists, so we can't simply expect to use something like count($lines) to
    count the number of elements in the ArrayList stored in $lines. I tried using
    the usual EXSLT functions and even the Xalan extensions to call Java methods,
    but I only got "Unknown error in XPath" exceptions one after another.
    
    What did work, however, was to simply detect this case, map the contents of
    the ArrayList into a DOM tree and return each element as a node in a node list.
    Since org.w3c.dom.NodeList is only an interface, this commit implements a
    simple alternative which is backed by an ArrayList (hence ArrayNodeList).
    
    All in all, I like this approach better: the use can simply ignore the problem
    above and merrily call "count($lines)" as if it was a regular node list.

---
+++ org.bpelunit.framework/src/org/bpelunit/framework/model/test/data/ArrayNodeList.java
@@ -0,0 +1,47 @@
+/**
+ * This file belongs to the BPELUnit utility and Eclipse plugin set. See enclosed
+ * license file for more information.
+ */
+package org.bpelunit.framework.model.test.data;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Simple NodeList implementation, backed by an ArrayList.
+ * 
+ * @author Antonio García-Domínguez
+ * @version 1.0
+ */
+public class ArrayNodeList implements NodeList, Iterable<Node> {
+
+    private ArrayList<Node> store = new ArrayList<Node>();
+
+    @Override
+    public int getLength() {
+        return store.size();
+    }
+
+    @Override
+    public Node item(int index) {
+        return index < store.size() ? store.get(index) : null;
+    }
+
+    /**
+     * Adds a Node at the end of the list.
+     * 
+     * @param n
+     *            Node to be added.
+     */
+    public void add(Node n) {
+        store.add(n);
+    }
+
+    @Override
+    public Iterator<Node> iterator() {
+        return store.iterator();
+    }
+}
---
+++ org.bpelunit.framework/src/org/bpelunit/framework/model/test/data/ContextXPathVariableResolver.java
@@ -0,0 +1,93 @@
+/**
+ * This file belongs to the BPELUnit utility and Eclipse plugin set. See enclosed
+ * license file for more information.
+ */
+package org.bpelunit.framework.model.test.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathVariableResolver;
+
+import org.apache.velocity.context.Context;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+/**
+ * XPath variable resolver backed by a Velocity context. Converts ArrayLists
+ * into NodeLists, by placing every element in the array into an XML <element>
+ * element. Elements in nested ArrayLists are recursively placed into nested
+ * XML <element> elements.
+ * 
+ * A requests for XPath variable $x will be converted into a request for the 'x'
+ * member of the Velocity context. Converted ArrayLists will be cached to improve
+ * performance.
+ * 
+ * @author Antonio García-Domínguez
+ */
+public class ContextXPathVariableResolver implements XPathVariableResolver {
+
+	private final Context templateContext;
+	private final Map<String, Object> conversionCache = new HashMap<String, Object>();
+
+	public ContextXPathVariableResolver(Context templateContext) {
+		this.templateContext = templateContext;
+	}
+
+	@Override
+	public Object resolveVariable(QName arg0) {
+		final String varName = arg0.getLocalPart();
+		final Object varValue = templateContext.get(varName);
+		if (varValue instanceof Iterable<?>) {
+			if (conversionCache.get(varName) != null) {
+				return conversionCache.get(varName);
+			}
+
+			NodeList nl = convertIterableToTree(varValue);
+			conversionCache.put(varName, nl);
+			return nl;
+		}
+		return varValue;
+	}
+
+	private NodeList convertIterableToTree(final Object varValue)
+			throws DOMException {
+		Document doc;
+		try {
+			doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+		} catch (ParserConfigurationException e) {
+			return null;
+		}
+
+		Element elem = doc.createElement("elements");
+		doc.appendChild(elem);
+		addElementsAsChildren((Iterable<?>)varValue, doc, elem);
+
+		// Just returning elem.getChildNodes() won't work: count($x) would
+		// return -1 in that case, for some reason.
+		ArrayNodeList nl = new ArrayNodeList();
+		NodeList children = elem.getChildNodes();
+		for (int i = 0; i < children.getLength(); ++i) {
+			nl.add(children.item(i));
+		}
+		return nl;
+	}
+
+	private void addElementsAsChildren(Iterable<?> collection, Document doc, Element currentElem) {
+		for (Object item : collection) {
+			Element childElem = doc.createElement("element");
+			currentElem.appendChild(childElem);
+			if (item instanceof Iterable<?>) {
+				addElementsAsChildren((Iterable<?>)item, doc, childElem);
+			} else {
+				childElem.setTextContent(item.toString());
+			}
+		}
+	}
+
+}
