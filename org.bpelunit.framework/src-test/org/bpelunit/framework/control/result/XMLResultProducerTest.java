790718dfc57470c273e6f4a499acd432b8ff1a1b
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2010-05-08 17:21:20 +0200

    XMLResultProducerTest: added test case for checking the contents of the results

--- org.bpelunit.framework/src-test/org/bpelunit/framework/control/result/XMLResultProducerTest.java
+++ org.bpelunit.framework/src-test/org/bpelunit/framework/control/result/XMLResultProducerTest.java
@@ -1,16 +1,34 @@
 package org.bpelunit.framework.control.result;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.xmlbeans.XmlOptions;
+import org.bpelunit.framework.model.test.report.ArtefactStatus;
+import org.bpelunit.framework.xml.result.XMLArtefact;
+import org.bpelunit.framework.xml.result.XMLData;
+import org.bpelunit.framework.xml.result.XMLInfo;
+import org.bpelunit.framework.xml.result.XMLPartnerTrack;
+import org.bpelunit.framework.xml.result.XMLReceiveCondition;
+import org.bpelunit.framework.xml.result.XMLTestCase;
+import org.bpelunit.framework.xml.result.XMLTestResult;
 import org.bpelunit.framework.xml.result.XMLTestResultDocument;
+import org.bpelunit.framework.xml.result.XMLData.XmlData;
+import org.bpelunit.framework.xml.result.XMLPartnerTrack.Activity;
+import org.bpelunit.framework.xml.result.XMLReceiveCondition.Condition;
 import org.bpelunit.test.end2end.End2EndTester;
 import org.bpelunit.test.util.StringOutputStream;
 import org.bpelunit.test.util.TestTestRunner;
 import org.junit.Test;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 /**
  * Tests for ensuring that the results produced by XMLResultProducer are valid
@@ -41,6 +59,161 @@ public class XMLResultProducerTest {
 			= new TestTestRunner("resources/result/InvalidAssumption/",
 					"WastePaperBasketTestSuite.bpts");
 		assertValid(getResults(runner));
+	}
+
+	/**
+	 * Test which performs several checks on the contents of an XML result file.
+	 * It is not intended to be complete, but rather just ensure that the
+	 * refactoring goes smoothly.
+	 */
+	@Test
+	public void testSendOnlyReceiveOnlyResultsAreComplete() throws Exception {
+		final String sPassed = ArtefactStatus.StatusCode.PASSED.toString();
+		final Map<String, String> passedState = new HashMap<String, String>();
+		passedState.put("Status Code", sPassed);
+		passedState.put("Status Message", "Passed");
+
+		// <testResult>
+		XMLTestResultDocument resDoc
+			= getResults(End2EndTester.getSendOnlyReceiveOnlyRunner());
+		XMLTestResult res;
+		assertNotNull(res = resDoc.getTestResult());
+		assertState(res, sPassed, passedState);
+
+		// <testCase>
+		List<XMLTestCase> testCases = res.getTestCaseList();
+		assertEquals("There should be results for 1 test case", 1, testCases.size());
+		XMLTestCase testCase = res.getTestCaseArray(0);
+		assertEquals("Test Case Throw something in!", testCase.getName());
+		assertState(testCase, sPassed, passedState);
+
+		// <partnerTrack>
+		List<XMLPartnerTrack> partnerTracks = testCase.getPartnerTrackList();
+		assertEquals("There should be 2 partner tracks", 2, partnerTracks.size());
+		final String partnerTrackNames[] = new String[] {
+				"Partner Track client", "Partner Track WastePaperBasket" };
+		for (int iPartnerTrack = 0; iPartnerTrack < partnerTracks.size(); ++iPartnerTrack) {
+			final XMLPartnerTrack partnerTrack = partnerTracks.get(iPartnerTrack);
+			final String sPartnerTrackName = partnerTrackNames[iPartnerTrack];
+			assertEquals(sPartnerTrackName, partnerTrack.getName());
+			assertState(partnerTrack, sPassed, passedState);
+
+			List<Activity> activities = partnerTrack.getActivityList();
+			assertEquals("Partner track " + sPartnerTrackName
+					+ " should have 1 activity", 1, activities.size());
+			Activity activity = activities.get(0);
+			assertState(activity, sPassed, passedState);
+
+			List<XMLData> dataPackages = activity.getDataPackageList();
+			assertEquals("Activity "
+					+ activity.getName() + " should have 1 data package",
+					1, dataPackages.size());
+			XMLData dataPackage = dataPackages.get(0);
+
+			if (iPartnerTrack == 0) {
+				checkDataPackage(dataPackage, true,
+						new String[]{"Literal XML data",
+						"SOAP Message data",
+						"Plain outgoing message"});
+
+				assertEquals(
+						"Send only activity should have no receive conditions",
+						0,
+						dataPackage.getReceiveConditionList().size());
+			} else {
+				checkDataPackage(dataPackage, false,
+						new String[]{"Plain incoming message",
+							"SOAP Message data",
+							"Literal XML data"});
+
+				// <receiveCondition>
+				List<XMLReceiveCondition> receiveConds
+					= dataPackage.getReceiveConditionList();
+				assertEquals(
+						"Receive only activity should only have 1 condition",
+						1,
+						receiveConds.size());
+
+				XMLReceiveCondition receiveCond = receiveConds.get(0);
+				final Map<String, String> condState
+					= new HashMap<String, String>();
+				final String expectedExpression
+					= "wpb:WastePaperBasketUsage/wpb:WasteThrower/text()";
+				final String expectedValue
+					= "'Phil'";
+
+				condState.put("Expression", expectedExpression);
+				condState.put("Value", expectedValue);
+				assertState(receiveCond, sPassed, passedState);
+				assertState(receiveCond, null, condState);
+
+				Condition cond = receiveCond.getCondition();
+				assertNotNull(cond);
+				assertEquals(expectedExpression, cond.getExpression());
+				assertEquals(expectedValue, cond.getExpectedValue());
+				assertEquals(null, cond.getActualValue());
+			}
+		}
+
+
+	}
+
+	private void checkDataPackage(XMLData dataPackage, boolean bIsSend,
+			String[] expectedXmlDataNames) {
+		final Map<String, String> directionState = new HashMap<String, String>();
+		directionState.put("Style/Encoding", "document/literal");
+		directionState.put("Direction", "INPUT");
+		final Map<String, String> sendState = new HashMap<String, String>();
+		sendState.put("Target URL", "http://localhost:7777/ws/WastePaperBasket");
+		sendState.put("HTTP Action", "initiate");
+
+		assertState(dataPackage, null, directionState);
+		if (bIsSend)
+			assertState(dataPackage, null, sendState);
+
+		List<XmlData> xmlDataList = dataPackage.getXmlDataList();
+		assertEquals("Data packages should have 3 <xmlData> elements",
+				3, xmlDataList.size());
+		for (int iData = 0; iData < xmlDataList.size(); ++iData) {
+			final XmlData xmlData = xmlDataList.get(iData);
+			assertEquals(expectedXmlDataNames[iData], xmlData.getName());
+			Element firstElement = getFirstChildElement(xmlData.getDomNode());
+
+			final String expectedName =
+				((bIsSend && iData == 0) || (!bIsSend && iData == 2))
+				? "literalData" : "SOAP-ENV:Envelope";
+			assertEquals(expectedName, firstElement.getNodeName());
+		}
+	}
+
+	private Element getFirstChildElement(Node domNode) {
+		for (Node nChild = domNode.getFirstChild();
+			nChild != null;
+			nChild = nChild.getNextSibling())
+		{
+			if (nChild instanceof Element) {
+				return (Element)nChild;
+			}
+		}
+		return null;
+	}
+
+	private void assertState(XMLArtefact artefact,
+			String expectedResult,
+			Map<String, String> expectedStateData) {
+		if (expectedResult != null) {
+			assertEquals(expectedResult, artefact.getResult());
+		}
+		for (XMLInfo info : artefact.getStateList()) {
+			final String key = info.getName();
+			if (expectedStateData.get(key) != null) {
+				assertEquals(expectedStateData.get(key), info.getStringValue());
+				expectedStateData.remove(key);
+			}
+		}
+		if (expectedStateData.size() != 0) {
+			fail("Not all expected state values were found");
+		}
 	}
 
 	private XMLTestResultDocument getResults(TestTestRunner runner) throws Exception {
