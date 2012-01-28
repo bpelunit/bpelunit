fa36a762459c0fbcd7efa15545572386982fc7cf
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2012-01-28 18:21:10 +0100

    fix new 'src' feature after merge

---
+++ net.bpelunit.framework/src/test/java/net/bpelunit/framework/DataSrcAttributeTest.java
@@ -0,0 +1,24 @@
+package net.bpelunit.framework;
+
+import java.io.File;
+
+import net.bpelunit.test.templates.AbstractTemplateTest;
+import net.bpelunit.test.util.TestUtil;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+public class DataSrcAttributeTest {
+
+	protected static final File TEST_BPTS_DIR = new File(
+			FileUtils.toFile(AbstractTemplateTest.class.getResource("/")),
+			"datasrc");
+
+	@Test
+	public void dataSrcWorks() throws Exception {
+		TestUtil.assertSameAndSuccessfulResults(
+				"Using external files or embedding the data in the BPTS should always produce the same results",
+				new File(TEST_BPTS_DIR, "BookSearch-4results-data.bpts"),
+				new File(TEST_BPTS_DIR, "BookSearch-4results-fi.bpts"));
+	}
+}
