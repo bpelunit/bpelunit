fa48139186e36ec6d475fe1af5bec443115a23a6
Daniel Luebke <bpelunit@daniel-luebke.de>
2012-04-26 21:30:12 +0200

    Remove unused import warning

--- net.bpelunit.framework.control.deploy.activevos9/src/main/java/net/bpelunit/util/XMLUtil.java
+++ net.bpelunit.framework.control.deploy.activevos9/src/main/java/net/bpelunit/util/XMLUtil.java
@@ -10,7 +10,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
