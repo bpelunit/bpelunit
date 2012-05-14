ad93ca6b9e06d6901b6d9ee6c0b01836f19bc5dc
Daniel Luebke <bpelunit@daniel-luebke.de>
2012-05-14 18:39:04 +0200

    Tests for EndpointChanger

--- net.bpelunit.framework.control.deploy.activevos9/src/test/java/net/bpelunit/framework/control/deploymentchanger/endpointreplacement/EndpointReplacerTest.java
+++ net.bpelunit.framework.control.deploy.activevos9/src/test/java/net/bpelunit/framework/control/deploymentchanger/endpointreplacement/EndpointReplacerTest.java
@@ -1,9 +1,10 @@
 package net.bpelunit.framework.control.deploymentchanger.endpointreplacement;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 
 import net.bpelunit.framework.control.deploy.DeploymentMock;
@@ -13,7 +14,6 @@ import net.bpelunit.framework.model.Partner;
 import net.bpelunit.framework.model.ProcessUnderTest;
 import net.bpelunit.framework.model.test.TestSuite;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -24,12 +24,54 @@ public class EndpointReplacerTest {
 	
 	@Test
 	public void testReplacementAllPartnersAreMockedSingleProcess() throws Exception {
+		TestSuite ts = createTestSuite();
+		addPartnerTrack(ts, "Provider");
+		addPartnerTrack(ts, "Provider2");
+		DeploymentMock deployment = new DeploymentMock("tc1.bpel");
 		
+		endpointReplacer.changeDeployment(deployment, ts);
+		
+		BPELProcessMock process = deployment.getBPELProcesses().get(0);
+		Map<String, String> changedEndpoints = process.getChangedEndpoints();
+		assertEquals(2, changedEndpoints.size());
+		assertEquals("http://localhost:7777/ws/Provider", changedEndpoints.get("Provider"));
+		assertEquals("http://localhost:7777/ws/Provider2", changedEndpoints.get("Provider2"));
 	}
 	
 	@Test
-	@Ignore("Not yet implemented")
 	public void testReplacementNotAllPartnersAreMockedSingleProcess() throws Exception {
+		TestSuite ts = createTestSuite();
+		addPartnerTrack(ts, "Provider");
+		addPartnerTrack(ts, "Provider2");
+		addPartnerTrack(ts, "ProviderUnused");
+		DeploymentMock deployment = new DeploymentMock("tc1.bpel");
+		
+		endpointReplacer.changeDeployment(deployment, ts);
+		
+		BPELProcessMock process = deployment.getBPELProcesses().get(0);
+		Map<String, String> changedEndpoints = process.getChangedEndpoints();
+		assertEquals(2, changedEndpoints.size());
+		assertEquals("http://localhost:7777/ws/Provider", changedEndpoints.get("Provider"));
+		assertEquals("http://localhost:7777/ws/Provider2", changedEndpoints.get("Provider2"));
+	}
+
+	private void addPartnerTrack(TestSuite ts, String name) throws SpecificationException {
+		Partner p = new Partner(name, null, null, BASE_URL);
+		ts.getProcessUnderTest().getPartners().put(name, p);
+	}
+
+	private TestSuite createTestSuite() throws SpecificationException,
+			MalformedURLException {
+		ProcessUnderTest put = new ProcessUnderTest("bpelName", ".", null, null, BASE_URL);
+		TestSuite ts = new TestSuite("test.bpts", new URL(BASE_URL), put);
+		
+		put.setPartners(new HashMap<String, Partner>());
+		
+		return ts;
+	}
+	
+	@Test
+	public void testReplacementNotAllPartnerLinksAreMockedSingleProcess() throws Exception {
 		TestSuite ts = createTestSuite();
 		addPartnerTrack(ts, "Provider");
 		DeploymentMock deployment = new DeploymentMock("tc1.bpel");
@@ -41,27 +83,27 @@ public class EndpointReplacerTest {
 		assertEquals(1, changedEndpoints.size());
 		assertEquals("http://localhost:7777/ws/Provider", changedEndpoints.get("Provider"));
 	}
-
-	private void addPartnerTrack(TestSuite ts, String name) throws SpecificationException {
-		Partner p = new Partner(name, null, null, BASE_URL);
-		ts.getProcessUnderTest().getPartners().put(name, p);
-	}
-
-	private TestSuite createTestSuite() throws SpecificationException,
-			MalformedURLException {
-		ProcessUnderTest suiteProcessUnderTest = new ProcessUnderTest("bpelName", ".", null, null, BASE_URL);
-		TestSuite ts = new TestSuite("test.bpts", new URL(BASE_URL), suiteProcessUnderTest);
-		return ts;
-	}
-	
-	@Test
-	public void testReplacementNotAllPartnerLinksAreMockedSingleProcess() throws Exception {
-		
-	}
 	
 	@Test
 	public void testReplacementAllPartnersAreMockedTwoProcesses() throws Exception {
+		TestSuite ts = createTestSuite();
+		addPartnerTrack(ts, "Provider");
+		addPartnerTrack(ts, "Provider2");
+		DeploymentMock deployment = new DeploymentMock("tc1.bpel", "tc1.bpel");
 		
+		endpointReplacer.changeDeployment(deployment, ts);
+		
+		BPELProcessMock process = deployment.getBPELProcesses().get(0);
+		Map<String, String> changedEndpoints = process.getChangedEndpoints();
+		assertEquals(2, changedEndpoints.size());
+		assertEquals("http://localhost:7777/ws/Provider", changedEndpoints.get("Provider"));
+		assertEquals("http://localhost:7777/ws/Provider2", changedEndpoints.get("Provider2"));		
+		
+		process = deployment.getBPELProcesses().get(1);
+		changedEndpoints = process.getChangedEndpoints();
+		assertEquals(2, changedEndpoints.size());
+		assertEquals("http://localhost:7777/ws/Provider", changedEndpoints.get("Provider"));
+		assertEquals("http://localhost:7777/ws/Provider2", changedEndpoints.get("Provider2"));
 	}
 	
 }
