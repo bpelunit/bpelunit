bc76ddfebbd204eb57269f3901aa6ab2f1b9ec4f
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2013-03-29 17:48:08 +0100

    build-all.sh: clean before building

--- build-all.sh
+++ build-all.sh
@@ -3,6 +3,6 @@
 # Abort build on first failure
 set +x
 
-mvn install
-mvn -f tycho/pom.xml install
-mvn -f net.bpelunit.dist/pom.xml install
+mvn clean install
+mvn -f tycho/pom.xml clean install
+mvn -f net.bpelunit.dist/pom.xml clean install
