a21efa03de6ace85d511d8fa1aad00c0bf37a318
Daniel Luebke <bpelunit@daniel-luebke.de>
2013-11-22 17:40:20 +0100

    Fix UI handling of unset header processor

--- tycho/net.bpelunit.toolsupport/src/net/bpelunit/toolsupport/editors/wizards/SendOnlyWizard.java
+++ tycho/net.bpelunit.toolsupport/src/net/bpelunit/toolsupport/editors/wizards/SendOnlyWizard.java
@@ -22,17 +22,15 @@ public class SendOnlyWizard extends ActivityWizard {
 	private XMLSendOnlyActivity fSendActivity;
 	private SendCompleteWizardPage fSendPage;
 	private HeaderProcessorPage fHeaderPage;
-	
-	
-	
+
 	public SendOnlyWizard(TestSuitePage page, ActivityEditMode mode, XMLSendOnlyActivity operation) {
 		super(page, mode);
-		fSendActivity= operation;
+		fSendActivity = operation;
 	}
 
 	@Override
 	public boolean performFinish() {
-		
+
 		transferOperation(fSendPage, fSendActivity);
 		transferFault(fSendPage.getSendFault(), fSendActivity);
 		transferFaultString(fSendPage.getSendFaultName(), fSendActivity);
@@ -40,22 +38,32 @@ public class SendOnlyWizard extends ActivityWizard {
 		transferLiteralSendData(fSendPage.getSendXML(), fSendActivity);
 		transferDelay(fSendPage.getDelaySequence(), fSendActivity);
 		fSendPage.getSendComponent().saveData();
+
+		if (!fHeaderPage.hasHeaderProcessorSelected()) {
+			// No header was selected. In this case, we have to remove
+			// the header processor.
+			// If a processor was selected, all data already belongs to the
+			// activity.
+			fSendActivity.unsetHeaderProcessor();
+		}
+
 		return true;
 	}
 
 	@Override
 	public void addPages() {
 		super.addPages();
-		fSendPage= new SendCompleteWizardPage(fSendActivity, fSendActivity, getMode(), getPageName());
+		fSendPage = new SendCompleteWizardPage(fSendActivity, fSendActivity, getMode(),
+				getPageName());
 		addPage(fSendPage);
 		addHeaderProcessorPage();
 	}
 
 	protected void addHeaderProcessorPage() {
-		fHeaderPage= new HeaderProcessorPage(fSendActivity, getMode(), getPageName());
+		fHeaderPage = new HeaderProcessorPage(fSendActivity, getMode(), getPageName());
 		addPage(fHeaderPage);
 	}
-	
+
 	@Override
 	protected String getPageName() {
 		return "Send Asynchronous";
