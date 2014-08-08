230d8df3f0aba668c56b180d8bf2cec715c91720
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2014-08-08 12:52:36 +0200

    build-all.sh: do not run the end-to-end ActiveBPEL test cases (fixes #10)

--- build-all.sh
+++ build-all.sh
@@ -3,6 +3,10 @@
 # Abort build on first failure
 set +x
 
+# Do not run the end-to-end ActiveBPEL test cases
+# (some devs may not have it set up).
+unset CATALINA_HOME
+
 mvn clean install
 mvn -f tycho/pom.xml clean install
 mvn -f net.bpelunit.dist/pom.xml clean install
